0ba5cc252a450c324666bbf29cb7db696a7d17dd
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2013-07-10 12:39:06 -0400

    EOD

--- config.php
+++ config.php
@@ -1,2 +1,9 @@
 <?php
 
+$config['BASE_APPLIC_DIRECTORY'] = 'applications';
+$config['BASE_SYSTEM_DIRECTORY'] = 'system';
+$config['BASE_VENDOR_DIRECTORY'] = 'vendor';
+
+$config['base_url'] = "http://domain.tld/";
+$config['index_page']   = "";
+$config['uri_protocol'] = "REQUEST_URI";
